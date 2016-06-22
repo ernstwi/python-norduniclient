3b077138d360ac91d2f7df1fbc69600d20a262ca
Johan Lundberg <lundberg@nordu.net>
2016-06-22 17:00:21 +0200

    Init

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='python-norduniclient',
+    version='0.1',
+    packages=['norduniclient'],
+    url='https://github.com/NORDUnet/python-norduniclient',
+    license='',
+    author='Johan Lundberg',
+    author_email='lundberg@nordu.net',
+    description='Neo4j database client for NORDUnet network inventory'
+)
