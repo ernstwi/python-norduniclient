907dfc04859c1d8ac4560bb20036b2a2ad798a99
Johan Lundberg <lundberg@sunet.se>
2017-09-01 12:52:12 +0200

    Deprecated docs

--- norduniclient/contextmanager.py
+++ norduniclient/contextmanager.py
@@ -14,17 +14,9 @@ class Neo4jDBSessionManager:
     Every new connection is a transaction. To minimize new connection overhead for many reads we try to reuse a single
     connection. If this seem like a bad idea some kind of connection pool might work better.
 
-    Neo4jDBSessionManager.read()
-    When using with Neo4jDBSessionManager.read(): we will always rollback the transaction. All exceptions will be
-    thrown.
-
-    Neo4jDBSessionManager.write()
-    When using with Neo4jDBSessionManager.write() we will always commit the transaction except when we see an
-    exception. If we get an exception we will rollback the transaction and throw the exception.
+    Neo4jDBSessionManager.session()
 
     Neo4jDBSessionManager.transaction()
-    When we don't want to share a connection (transaction context) we can set up a new connection which will work
-    just as the write context manager above but with it's own connection.
     """
 
     def __init__(self, uri, username=None, password=None, encrypted=True):
